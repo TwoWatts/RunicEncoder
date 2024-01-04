179470f5ec6631219998316471946bcce7835346
TwoWatts <therzog.bus@gmail.com>
2024-01-03 18:53:59 -0600

    Refine CLI and prevent user from providing empty strings as program input.

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,6 @@ fn main() {
         eprintln!("Failed to get the current directory.");
     }
 
-    // // Delete a directory
-    // let dir_to_delete = "lang"; // Specify the directory path to delete
-    // if let Err(err) = fs::remove_dir_all(dir_to_delete) {
-    //     eprintln!("Failed to delete directory: {}", err);
-    // } 
-
-    // // Create a directory
-    // let dir_to_create = "lang"; // Specify the new directory path
-    // if let Err(err) = fs::create_dir(dir_to_create) {
-    //     eprintln!("Failed to create directory: {}", err);
-    // }
-
     // Menu Prompt
     println!("Select an option:");
     println!("[0] Encode rune.");
@@ -36,7 +24,10 @@ fn main() {
     // Accept Input
     let mut option = String::new();
     io::stdin().read_line(&mut option).expect("ERROR: Could not read input.");
-    let answer: u32= option.trim().parse().unwrap();
+    if option.trim().is_empty() {
+        return;
+    }
+    let answer: u32 = option.trim().parse().unwrap();
     
     if answer == 0 {
         println!("Encode your rune by entering text and pressing <ENTER>...");
@@ -45,9 +36,15 @@ fn main() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("ERROR: Could not read input.");
-        let text = input.as_str().trim();
+        let mut text = input.as_str().trim();
+        while text == "" {
+            print!(">> ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut input).expect("ERROR: Could not read input.");
+            text = input.as_str().trim();
+        }
         let text_bytes = text.as_bytes();
-        //println!("You entered: {:?}", text);
+        //println!("You entered: {:?}", text_bytes);
         println!("Rune Succeeded! See \"{}.png\"...", text);
         let grid: Vec<Rgb<u8>> = mat::load_image_grid("res/base16-mat.png", 18, 18, 1);
         mat::create_mat(grid, 16, 16, text_bytes, text);
